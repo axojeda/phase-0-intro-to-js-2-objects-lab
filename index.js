b0f86ac21a2443dec855045fe13ba6fd12384295
Aura <ax.pink@gmail.com>
2022-12-03 09:51:14 -0800

    commit tree ffb66efbdd19f1c1320f503c2acdc685c625ede2

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var employee = {}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    var newObj = Object.assign({}, obj)
+    newObj[key] = value
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    var newObj = Object.assign({}, obj)
+    delete newObj[key]
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
